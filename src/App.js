6397510913291d0400a901ddd0670fb75759934f
shambhu_bcoder <shambhunath@bcoder.in>
2023-03-06 10:33:32 +0530

    other changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import React from 'react';
 import Header from "./Components/Header/Header"
-
+import Code from "./Components/Code/Code.jsx";
 function App() {
   return (
     <div className="App">
-
       <Header />
+      <Code />
     </div>
   );
 }
+
+// const style = {
+//   padding: "20px",
+//   marginLeft: "30px",
+//   border: "2px solid black",
+//   display: "inline-block"
+// }
+// const User = (props) => {
+//   return <div style={style}>
+//     <label for="userInfo" style={{
+//       textAlign: "center",
+//       margin: "0,auto",
+//     }}>User Info</label>
+//     <p>Name :{props.name}</p>
+//     <p>Age :{props.age}</p>
+//     <p>Place :{props.place}</p>
+
+//   </div>
+// }
+
 
 export default App;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 function Header() {
     return (
 
-        <nav class="navbar navbar-expand-lg bg-dark">
+        <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand " href="#/">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
